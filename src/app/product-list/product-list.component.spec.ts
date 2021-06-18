f234765695fbd7cd4e7dd16e448375a8b390d371
Saibal Roy <saibal@technoexponent.com>
2021-06-18 14:43:15 +0530

    wip

---
+++ src/app/product-list/product-list.component.spec.ts
@@ -0,0 +1,31 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductListComponent } from './product-list.component';
+
+describe('ProductListComponent', () => {
+  let component: ProductListComponent;
+  let fixture: ComponentFixture<ProductListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProductListComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProductListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should have 'Product List Component' as title`, async(() => {
+  fixture = TestBed.createComponent(ProductListComponent);
+  component = fixture.debugElement.componentInstance;
+  expect(component.title).toEqual('Product List Component');
+}));
+});
